b1d7850d8b0ab6b14aa8d8dea147adb169196938
Caesar Lennartsson <caesar.1999@hotmail.com>
2022-01-23 21:50:26 +0100

    Add state and functions for event

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import logo from '../assets/logo.png'
 
 function StartPage() {
+  const [username, setUsername] = useState<string>('')
+
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value)
+  }
+
+  const handleOnSubmit = () => {
+    console.log('submit: ', username)
+  }
+
   return (
     <div className='w-3/4 container mx-auto h-full flex flex-col items-center'>
       {/* image */}
@@ -18,10 +28,14 @@ function StartPage() {
           type='text'
           name='username'
           id='username'
+          placeholder='Username...'
+          value={username}
+          onChange={handleOnChange}
           className='text-white border bg-black py-3 px-4 custom-github-input mr-2 block w-full text-sm border-gray-700 rounded-md focus:outline-none'
         />
         {/* submit button */}
         <button
+          onClick={handleOnSubmit}
           type='submit'
           className='w-1/4 items-center justify-center py-2 px-4 text-sm font-medium rounded-md text-white bg-green-600 custom-github-button hover:bg-green-500'
         >
