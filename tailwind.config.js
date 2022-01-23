f1c792e333589e2ceabc78bfb8710dab59e331d4
Caesar Lennartsson <caesar.1999@hotmail.com>
2022-01-23 21:14:10 +0100

    Add logo and update lint rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black': '#000000',
+      },
+    },
   },
   plugins: [],
 }
