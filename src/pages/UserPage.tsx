bf056d21b066a7fdb02c7a07d5556275a409bdc4
Caesar Lennartsson <caesar.1999@hotmail.com>
2022-01-24 23:13:40 +0100

    Update user page for fetch repos and add interface

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,21 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import APIService from '../services/APIService'
+import IRepo from '../types/IRepo'
 import IUserInfo from '../types/IUserInfo'
 
 function UserPage() {
   const [user, setUser] = useState<IUserInfo | undefined>(undefined)
+  const [repos, setRepos] = useState([])
   const { username } = useParams()
 
   useEffect(() => {
     const fetchData = async () => {
       if (!username) return
-      const [response, error]: any = await APIService.findByUsername(username)
-      if (error) {
+      //const [response, error]: any = await APIService.findByUsername(username)
+      const values = await Promise.all([
+        APIService.findByUsername(username),
+        APIService.findAllReposByUser(username),
+      ])
+      if (
+        values[0][1] ||
+        values[1][1] ||
+        values[0][0] == null ||
+        values[1][0] == null
+      ) {
+        const error = values[0][1] || values[1][1]
         console.log(error)
         return
       }
-      setUser(response.data)
+      setUser(values[0][0].data)
+      setRepos(values[1][0].data)
     }
 
     /* /users/{username}/repos */
@@ -26,37 +39,29 @@ function UserPage() {
     <div className='w-full container mx-auto h-full flex flex-col items-center'>
       {/* header */}
       <div className='w-full border-b-1 flex py-3'>
-        {/* header - profile */}
         <p className='text-2xl text-slate-200 w-1/3'>Profile</p>
-        {/* header - repos */}
         <p className='text-2xl text-slate-200 w-2/3'>Repositories</p>
       </div>
       {/* container */}
       <div className='w-full flex py-3'>
         {/* left - profile */}
         <div className='w-1/3'>
-          {/* profile - avatar */}
           <img src={user?.avatar_url} />
-          {/* profile - fullname */}
           <p>{user?.name}</p>
-          {/* profile - username */}
           <p>{user?.login}</p>
-          {/* profile - bio */}
           <p>{user?.bio}</p>
-          {/* profile - github profile btn */}
           <button>{user?.html_url}</button>
-          {/* profile - company */}
           <p>{user?.company}</p>
-          {/* profile - location */}
           <p>{user?.location}</p>
-          {/* profile - website */}
           <p>{user?.blog}</p>
-          {/* profile - twitter */}
           <p>{user?.twitter_username}</p>
         </div>
 
         {/* right - repos */}
         <div className='w-2/3 flex py-3'>
+          {repos.map((repo: IRepo) => (
+            <p key={repo.html_url}>{repo.html_url}</p>
+          ))}
           {/* repo - row */}
           {/* name */}
           {/* language */}
---
+++ src/types/IRepo.ts
@@ -0,0 +1,10 @@
+interface IRepo {
+  html_url: string,
+  language: string,
+  name: string,
+  open_issues: number, //open_issues_count
+  updated_at: string,
+  license: string
+}
+
+export default IRepo
