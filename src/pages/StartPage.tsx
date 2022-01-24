c2612daa3105d97784f38eea99def1b3f7172646
Caesar Lennartsson <caesar.1999@hotmail.com>
2022-01-24 14:40:09 +0100

    Call API service on submit and display response

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,15 +1,31 @@
 import React, { ChangeEvent, useState } from 'react'
 import logo from '../assets/logo.png'
+import APIService from '../services/APIService'
+
+interface IUser {
+  id: number
+  avatar_url: string
+  html_url: string
+  repos_url: string
+  type: string
+  login: string
+}
 
 function StartPage() {
   const [username, setUsername] = useState<string>('')
+  const [users, setUsers] = useState([])
 
   const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
     setUsername(e.target.value)
   }
 
-  const handleOnSubmit = () => {
-    console.log('submit: ', username)
+  const handleOnSubmit = async () => {
+    const [response, error]: any = await APIService.searchOnUsername(username)
+    if (error) {
+      console.log('error', error)
+      return
+    }
+    setUsers(response.data.items)
   }
 
   return (
@@ -37,16 +53,48 @@ function StartPage() {
         <button
           onClick={handleOnSubmit}
           type='submit'
-          className='w-1/4 items-center justify-center py-2 px-4 text-sm font-medium rounded-md text-white bg-green-600 custom-github-button hover:bg-green-500'
-        >
+          className='w-1/4 items-center justify-center py-2 px-4 text-sm font-medium rounded-md text-white bg-green-600 custom-github-button hover:bg-green-500'>
           Search
         </button>
       </div>
-      {/* result container */}
-      {/* result row */}
-      {/* result row avatar */}
-      {/* result row username */}
-      {/* result row type */}
+      {/* list */}
+      <div className='w-3/4'>
+        <p className='text-slate-200 text-2xl mb-4'>
+          {users.length} users found
+        </p>
+        {users.map((person: IUser) => {
+          return (
+            <div
+              key={person.id}
+              className='flex items-center bg-slate-200 mb-2 rounded-md p-3'>
+              <div className='flex-item h-10 mr-4'>
+                <img
+                  className='h-10 w-10 rounded-full'
+                  src={person.avatar_url}
+                  alt='User avatar'
+                />
+              </div>
+              <div className='mr-4 flex-item-20'>
+                <div className='text-sm font-medium text-gray-900'>
+                  {person.login}
+                </div>
+              </div>
+              <div className='mr-4 flex-item'>
+                <div className='text-sm text-gray-500 '>{person.type}</div>
+              </div>
+              <div className='mr-4 flex-item-20 text-right'>
+                <a
+                  target='_blank'
+                  rel='noreferrer'
+                  href={person.html_url}
+                  className='text-sm font-medium text-indigo-600 hover:text-indigo-900'>
+                  Github Profile
+                </a>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
