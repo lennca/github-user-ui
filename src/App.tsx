10693924778645fb23d6c5ea7d1bd6e380a99437
Caesar Lennartsson <caesar.1999@hotmail.com>
2022-01-24 21:28:51 +0100

    Add routes to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import StartPage from './pages/StartPage'
+import UserPage from './pages/UserPage'
 
 function App() {
   return (
     <div className='App bg-gray-900'>
-      <StartPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<StartPage />} />
+          <Route path='/user/:username' element={<UserPage />} />
+        </Routes>
+      </BrowserRouter>
+      ,
     </div>
   )
 }
