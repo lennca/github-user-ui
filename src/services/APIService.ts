609da7c13daa0050d98b1bd6252d93ecd42fae67
Caesar Lennartsson <caesar.1999@hotmail.com>
2022-01-24 18:49:53 +0100

    Add API service function to get specific user

--- src/services/APIService.ts
+++ src/services/APIService.ts
@@ -3,11 +3,20 @@ import axios from 'axios'
 const baseURL = process.env.BASE_URL || 'https://api.github.com'
 
 const APIService = {
+  // better name 'findAllByUsername'?
   searchOnUsername: async (username: string) => {
     try {
       const response = await axios.get(
         `${baseURL}/search/users?q=${username}+in:user`
       )
+      return [response, null]
+    } catch (error) {
+      return [null, error]
+    }
+  },
+  findByUsername: async (username: string) => {
+    try {
+      const response = await axios.get(`${baseURL}/users/${username}`)
       return [response, null]
     } catch (error) {
       return [null, error]
