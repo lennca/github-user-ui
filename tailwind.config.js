a4a0aaf4dad7babc6286772268b08ceff91e09f2
Caesar Lennartsson <caesar.1999@hotmail.com>
2022-01-26 22:31:47 +0100

    Add colors schemas for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     extend: {
       colors: {
         'black': '#000000',
+        github: {
+          bg: '#0d1117',
+          primary: '#c9d1d9',
+          secondary: '#8b949e',
+          btn: '#238636',
+          btn_hover: '#2ea043',
+          btn_sec: '#21262d'
+        }
       },
     },
   },
