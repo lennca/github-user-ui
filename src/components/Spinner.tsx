0148ff476f9333b2dd1798063d89b98d1ba320d5
Caesar Lennartsson <caesar.1999@hotmail.com>
2022-01-27 21:57:34 +0100

    Add loading spinner when fetch user info

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Spinner() {
+  return (
+    <div className='flex justify-center items-center absolute top-1/2 left-1/2'>
+      <div className='animate-spin w-10 h-10 rounded-full spin'>
+      </div>
+    </div>
+  )
+}
+
+export default Spinner
--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -6,11 +6,13 @@ import IUserInfo from '../types/IUserInfo'
 import { LocationMarkerIcon, LinkIcon, OfficeBuildingIcon, ChatAltIcon } from '@heroicons/react/outline'
 import RepoRow from '../components/RepoRow'
 import InfoRow from '../components/InfoRow'
+import Spinner from '../components/Spinner'
 
 function UserPage() {
   const [user, setUser] = useState<IUserInfo | undefined>(undefined)
   const [repos, setRepos] = useState([])
   const { username } = useParams()
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,6 +26,7 @@ function UserPage() {
       }
       setUser(values[0][0].data)
       setRepos(values[1][0].data)
+      setLoading(false)
     }
 
     fetchData()
@@ -36,38 +39,41 @@ function UserPage() {
         <p className='text-4xl text-github-primary w-1/3'>Profile</p>
         <p className='text-4xl text-github-primary w-2/3'>Repositories [{repos.length}]</p>
       </div>
-      {/* container */}
-      <div className='w-full flex py-3 flex-col sm:flex-row'>
-        {/* left - profile */}
-        <div className='w-full sm:w-1/3 flex flex-col'>
-          <div className='flex sm:flex-col'>
-            <img className='rounded-full w-1/5 sm:w-4/5 mb-3 mr-3 sm:mr-0' src={user?.avatar_url} />
-            <div className='mb-3 flex flex-col justify-center flex-1'>
-              <p className='text-xl lg:text-3xl text-github-primary font-semibold'>{user?.name}</p>
-              <p className='text-xl text-github-secondary'>{user?.login}</p>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className='w-full flex py-3 flex-col sm:flex-row'>
+          {/* left - profile */}
+          <div className='w-full sm:w-1/3 flex flex-col'>
+            <div className='flex sm:flex-col'>
+              <img className='rounded-full w-1/5 sm:w-4/5 mb-3 mr-3 sm:mr-0' src={user?.avatar_url} />
+              <div className='mb-3 flex flex-col justify-center flex-1'>
+                <p className='text-xl lg:text-3xl text-github-primary font-semibold'>{user?.name}</p>
+                <p className='text-xl text-github-secondary'>{user?.login}</p>
+              </div>
             </div>
+            <p className='text-github-primary mb-3'>{user?.bio}</p>
+            <a
+              className='py-1 mb-3 github-btn-sec text-sm w-3/4 text-center'
+              href={user?.html_url}
+              target='_blank'
+              rel='noreferrer'>
+              Github Profile
+            </a>
+            <InfoRow text={user?.company || '-'} Element={OfficeBuildingIcon} link={false} />
+            <InfoRow text={user?.location || '-'} Element={LocationMarkerIcon} link={false} />
+            <InfoRow text={user?.blog || '-'} Element={LinkIcon} link={true} />
+            <InfoRow text={user?.twitter_username || '-'} Element={ChatAltIcon} link={true} />
           </div>
-          <p className='text-github-primary mb-3'>{user?.bio}</p>
-          <a
-            className='py-1 mb-3 github-btn-sec text-sm w-3/4 text-center'
-            href={user?.html_url}
-            target='_blank'
-            rel='noreferrer'>
-            Github Profile
-          </a>
-          <InfoRow text={user?.company || '-'} Element={OfficeBuildingIcon} link={false} />
-          <InfoRow text={user?.location || '-'} Element={LocationMarkerIcon} link={false} />
-          <InfoRow text={user?.blog || '-'} Element={LinkIcon} link={true} />
-          <InfoRow text={user?.twitter_username || '-'} Element={ChatAltIcon} link={true} />
-        </div>
 
-        {/* right - repos */}
-        <div className='w-full sm:w-2/3 flex flex-col'>
-          {repos.map((repo: IRepo) => (
-            <RepoRow repo={repo} key={repo.id} />
-          ))}
+          {/* right - repos */}
+          <div className='w-full sm:w-2/3 flex flex-col'>
+            {repos.map((repo: IRepo) => (
+              <RepoRow repo={repo} key={repo.id} />
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
