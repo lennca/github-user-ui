83ee19021da503b2deed8df71712c9b1fbfb5488
Caesar Lennartsson <caesar.1999@hotmail.com>
2022-02-03 21:28:26 +0100

    Update tailwind styling configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,33 @@ module.exports = {
     extend: {
       colors: {
         'black': '#000000',
-        github: {
-          bg: '#0d1117',
-          primary: '#c9d1d9',
-          secondary: '#8b949e',
-          btn: '#238636',
-          btn_hover: '#2ea043',
-          btn_sec: '#21262d',
-          border: '#30363d',
-          blue: '#58a6ff'
+        blue: {
+          light: '#58A6FF',
+          medium: '#1f6feb',
+          dark: '#0C2D6B',
+        },
+        green: {
+          light: '#2EA043',
+          medium: '#238636',
+          outline: '#00800040'
+        },
+        grey: {
+          1: '#C9D1D9',
+          2: '#8B949E',
+          5: '#373B42',
+          6: '#30363D',
+          7: '#21262D',
+          8: '#0D1117',
+          9: '#010409'
         }
       },
+      flex: {
+        '20': '1 1 20%'
+      },
+      boxShadow: {
+        'green': '0 0 0 3px #00800040',
+        'blue': '0 0 0 3px #0c2d6b'
+      }
     },
   },
   plugins: [],
